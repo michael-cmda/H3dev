176e6a9ba98f19c6c1dcdaaa70de325fd66c17ab
_bckDev <vlagria3@gmail.com>
2024-01-21 17:53:03 +0800

    test

--- lib/homePage/homePage.dart
+++ lib/homePage/homePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,26 +10,26 @@ class MyHomePage extends StatelessWidget {
         title: Row(
           children: [
             Image.asset('assets/images/logo.jpg', height: 30.0, width: 30.0),
-            SizedBox(width: 8.0),
-            Text('CITY LOADS'),
+            const SizedBox(width: 8.0),
+            const Text('CITY LOADS'),
           ],
         ),
         actions: [
           Container(
-            margin: EdgeInsets.only(right: 8.0),
+            margin: const EdgeInsets.only(right: 8.0),
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(20.0),
             ),
             child: IconButton(
-              icon: Icon(Icons.search),
+              icon: const Icon(Icons.search),
               onPressed: () {},
             ),
           ),
           Container(
-            margin: EdgeInsets.only(right: 16.0),
+            margin: const EdgeInsets.only(right: 16.0),
             child: IconButton(
-              icon: CircleAvatar(
+              icon: const CircleAvatar(
                 backgroundImage: AssetImage('assets/images/ChaHae.jpg'),
               ),
               onPressed: () {},
@@ -36,7 +38,7 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: ClipRRect(
-        borderRadius: BorderRadius.only(
+        borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(20.0),
           bottomLeft: Radius.circular(20.0),
         ),
@@ -45,77 +47,77 @@ class MyHomePage extends StatelessWidget {
             Container(
               width: 200.0,
               color: Colors.white,
-              padding: EdgeInsets.only(left: 16.0, bottom: 16.0),
+              padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 16.0),
+                  const SizedBox(height: 16.0),
                   GestureDetector(
                     onTap: () {
                       Navigator.of(context).pushReplacement(
                         MaterialPageRoute(
-                          builder: (context) => HomeRoute(),
+                          builder: (context) => const HomeRoute(),
                         ),
                       );
                     },
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Home'),
                       leading: Icon(Icons.home),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Search'),
                       leading: Icon(Icons.search),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Discover'),
                       leading: Icon(Icons.explore),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Latest News'),
                       leading: Icon(Icons.new_releases),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Create a Post'),
                       leading: Icon(Icons.edit),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Notification'),
                       leading: Icon(Icons.notifications),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Messages'),
                       leading: Icon(Icons.message),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Profile'),
                       leading: Icon(Icons.person),
                     ),
                   ),
                   GestureDetector(
                     onTap: () {},
-                    child: ListTile(
+                    child: const ListTile(
                       title: Text('Settings'),
                       leading: Icon(Icons.settings),
                     ),
@@ -126,8 +128,8 @@ class MyHomePage extends StatelessWidget {
             Expanded(
               child: Container(
                 color: Colors.grey[200],
-                padding: EdgeInsets.all(16.0),
-                child: Center(
+                padding: const EdgeInsets.all(16.0),
+                child: const Center(
                   child: Text('THIS IS HOME PAGE'),
                 ),
               ),
@@ -140,13 +142,15 @@ class MyHomePage extends StatelessWidget {
 }
 
 class HomeRoute extends StatelessWidget {
+  const HomeRoute({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: const Text('Home'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('This is the Home route.'),
       ),
     );

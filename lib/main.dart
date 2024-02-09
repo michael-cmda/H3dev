4db9afdeabf93c5b017ceab28cc2ee81f77b35da
michael-cmda <kaizarscore12@gmail.com>
2024-02-09 10:46:06 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:h3devs/chatsupport.dart';
 import 'package:h3devs/firebase_options.dart';
 import 'package:h3devs/homePage/homePage.dart';
+
 import 'package:h3devs/login.dart';
-import 'package:h3devs/register.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +16,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,24 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
-      home: Login(),
-=======
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else {
-            if (snapshot.hasData) {
-              return const MyHomePage();
-            } else {
-              return const Login();
-            }
-          }
-        },
-      ),
->>>>>>> 4f165e1a3477a14d9468b7009725dc0a201f06b9
+      home: UserSelectionPage(),
     );
   }
 }

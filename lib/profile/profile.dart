b30a3a8143ba72d81dd8b590fc211b0c069e23cc
michael-cmda <kaizarscore12@gmail.com>
2024-02-10 20:09:31 +0800

    init

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,1 +1,84 @@
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  late User? _currentUser;
+  Uint8List? _imageBytes;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUser();
+  }
+
+  Future<void> _getCurrentUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await user.reload(); // Reload the user to fetch updated data
+      setState(() {
+        _currentUser = FirebaseAuth.instance.currentUser;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: _currentUser != null
+          ? Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _currentUser!.photoURL != null
+                      ? CircleAvatar(
+                          radius: 80,
+                          backgroundImage: NetworkImage(_currentUser!.photoURL!),
+                        )
+                      : Container(
+                          height: 160,
+                          width: 160,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[200],
+                          ),
+                          child: Icon(Icons.person, size: 100, color: Colors.grey),
+                        ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Name: ${_currentUser!.displayName ?? 'N/A'}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Email: ${_currentUser!.email}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 12),
+                ],
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
--- lib/responsive/deskktop_body.dart
+++ lib/responsive/deskktop_body.dart
@@ -5,8 +5,8 @@ import 'package:h3devs/discover/discover.dart';
 import 'package:h3devs/homePage/homePage.dart';
 import 'package:h3devs/latestNews/latestNews.dart';
 import 'package:h3devs/messages/screens/messages.dart';
-import 'package:h3devs/messages/screens/recentMessages.dart';
 import 'package:h3devs/notification/notificationDrawer.dart';
+import 'package:h3devs/profile/profile.dart';
 import 'package:h3devs/search/searchDrawer.dart';
 
 class MyDesktopBody extends StatelessWidget {
@@ -367,10 +367,8 @@ class HomeMessagesWidget extends StatelessWidget {
             borderRadius: BorderRadius.circular(26.0),
             color: Colors.white,
           ),
-          child: const Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: RecentMessages(),
-          ),
+
+          // Add your child widget(s) here
         ),
       );
     } else {
@@ -447,20 +445,20 @@ class Sidebar extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 6.0),
-              // GestureDetector(
-              //   onTap: () {
-              //     Navigator.of(context).pushReplacement(
-              //       MaterialPageRoute(
-              //         builder: (context) => ListingImagesPage(),
-              //       ),
-              //     );
-              //   },
-              //   child: const ListTile(
-              //     minLeadingWidth: 30,
-              //     title: Text('Latest News'),
-              //     leading: Icon(Icons.new_releases),
-              //   ),
-              // ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => MyHomePage(),
+                    ),
+                  );
+                },
+                child: const ListTile(
+                  minLeadingWidth: 30,
+                  title: Text('Latest News'),
+                  leading: Icon(Icons.new_releases),
+                ),
+              ),
               const SizedBox(height: 10.0),
               GestureDetector(
                 onTap: () {
@@ -517,7 +515,11 @@ class Sidebar extends StatelessWidget {
               ),
               const SizedBox(height: 6.0),
               GestureDetector(
-                onTap: () {},
+                onTap: () {Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                  },
                 child: const ListTile(
                   minLeadingWidth: 30,
                   title: Text('Profile'),
